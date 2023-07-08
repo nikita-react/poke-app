5ff16e9b998f0ab8d19dbe00cb9a623722a20df9
Nikita Voskoboinikov <nikitavoskoboinikov2828@gmail.com>
2023-07-08 23:39:01 +0300

    finished write tests for table

--- src/components/MUIComponents/MUITable/MUITable.test.tsx
+++ src/components/MUIComponents/MUITable/MUITable.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen, fireEvent, queryAllByText, queryAllByTestId } from '@testing-library/react';
 import MUITable from '../MUITable';
-import {RenderPokemonsColumns, RenderSelectedPokemonsColumns} from "../../../constants";
+import { RenderPokemonsColumns, RenderSelectedPokemonsColumns } from "../../../constants";
 import { BrowserRouter } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import userEvent from '@testing-library/user-event';
+import PokemonsPage from '../../PokemonsPage';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom") as any,
@@ -10,219 +14,219 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe('MUITable', () => {
-    const data = {
-        pokemon_v2_pokemon: [
-        {
+  const data = {
+    pokemon_v2_pokemon: [
+      {
         id: 1,
         name: "bulbasaur",
         height: 7,
         base_experience: 64,
         is_default: true
-        },
-        {
+      },
+      {
         id: 2,
         name: "ivysaur",
         height: 10,
         base_experience: 142,
         is_default: true
-        },
-        {
+      },
+      {
         id: 3,
         name: "venusaur",
         height: 20,
         base_experience: 263,
         is_default: true
-        },
-        {
+      },
+      {
         id: 4,
         name: "charmander",
         height: 6,
         base_experience: 62,
         is_default: true
-        },
-        {
+      },
+      {
         id: 5,
         name: "charmeleon",
         height: 11,
         base_experience: 142,
         is_default: true
-        },
-        {
+      },
+      {
         id: 6,
         name: "charizard",
         height: 17,
         base_experience: 267,
         is_default: true
-        },
-        {
+      },
+      {
         id: 7,
         name: "squirtle",
         height: 5,
         base_experience: 63,
         is_default: true
-        },
-        {
+      },
+      {
         id: 8,
         name: "wartortle",
         height: 10,
         base_experience: 142,
         is_default: true
-        },
-        {
+      },
+      {
         id: 9,
         name: "blastoise",
         height: 16,
         base_experience: 265,
         is_default: true
-        },
-        {
+      },
+      {
         id: 10,
         name: "caterpie",
         height: 3,
         base_experience: 39,
         is_default: true
-        }
-        ],
-        pokemon_v2_pokemon_aggregate: {
-        aggregate: {
+      }
+    ],
+    pokemon_v2_pokemon_aggregate: {
+      aggregate: {
         count: 1279
-        }
-        }
-        }
-        
-        const handleChangeSelectedItems = jest.fn();
+      }
+    }
+  }
+
+  const handleChangeSelectedItems = jest.fn();
 
   test('renders checkboxes when renderCheckbox is true', () => {
-    
+
 
     render(
-        <BrowserRouter>
-            <MUITable
-              columns={RenderPokemonsColumns}
-              showPagination={true}
-              data={data}
-              renderCheckbox={true} 
-              renderDeleteButton={false}
-              handleChangeSelectedItems={handleChangeSelectedItems}
-            />
-            </BrowserRouter>
-    );
-
-    const checkboxes = screen.queryAllByTestId('checkbox');
-    expect(checkboxes).toHaveLength(data.pokemon_v2_pokemon.length);
-  
-  });
-  
-  test('does not render checkboxes when renderCheckbox is false', () => {
-    
-
-    render(
-        <BrowserRouter>
+      <BrowserRouter>
         <MUITable
           columns={RenderPokemonsColumns}
           showPagination={true}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={true}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
         />
-        </BrowserRouter>
+      </BrowserRouter>
+    );
+
+    const checkboxes = screen.queryAllByTestId('checkbox');
+    expect(checkboxes).toHaveLength(data.pokemon_v2_pokemon.length);
+
+  });
+
+  test('does not render checkboxes when renderCheckbox is false', () => {
+
+
+    render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          handleChangeSelectedItems={handleChangeSelectedItems}
+        />
+      </BrowserRouter>
     );
 
     const checkboxes = screen.queryAllByTestId('checkbox');
     expect(checkboxes).toHaveLength(0);
   });
 
-  test('render delete button' , () => {
-    
+  test('render delete button', () => {
+
 
     render(
-        <BrowserRouter>
+      <BrowserRouter>
         <MUITable
           columns={RenderPokemonsColumns}
           showPagination={true}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={true}
           handleChangeSelectedItems={handleChangeSelectedItems}
         />
-        </BrowserRouter>
+      </BrowserRouter>
     );
 
     const deleteButtons = screen.queryAllByTestId('delete-button');
     expect(deleteButtons).toHaveLength(data.pokemon_v2_pokemon.length);
   })
 
-  test('doesnt render delete button' , () => {
-    
+  test('doesnt render delete button', () => {
+
 
     render(
-        <BrowserRouter>
+      <BrowserRouter>
         <MUITable
           columns={RenderPokemonsColumns}
           showPagination={true}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
         />
-        </BrowserRouter>
+      </BrowserRouter>
     );
 
     const deleteButtons = screen.queryAllByTestId('delete-button');
     expect(deleteButtons).toHaveLength(0);
   })
 
-  test('show pagination' , () => {
-    
+  test('show pagination', () => {
+
 
     render(
-        <BrowserRouter>
+      <BrowserRouter>
         <MUITable
           columns={RenderPokemonsColumns}
           showPagination={true}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
         />
-        </BrowserRouter>
+      </BrowserRouter>
     );
     const pagination = screen.queryByTestId('table-pagination');
     expect(pagination).toBeInTheDocument();
   })
 
-  test(' doesnt show pagination' , () => {
-    
+  test(' doesnt show pagination', () => {
+
 
     render(
-        <BrowserRouter>
+      <BrowserRouter>
         <MUITable
           columns={RenderPokemonsColumns}
           showPagination={false}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
         />
-        </BrowserRouter>
+      </BrowserRouter>
     );
     const pagination = screen.queryByTestId('table-pagination');
     expect(pagination).not.toBeInTheDocument();
   })
 
   test('should render Skeleton when isFetching is true', () => {
-    const { queryByText} = render(
+    const { queryByText } = render(
       <BrowserRouter>
-      <MUITable
-        columns={RenderPokemonsColumns}
+        <MUITable
+          columns={RenderPokemonsColumns}
           showPagination={false}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
           isFetching={true}
-      />
+        />
       </BrowserRouter>
     );
 
@@ -253,17 +257,17 @@ describe('MUITable', () => {
 
   test('should render real data when isFetching is false', () => {
 
-    const { getAllByText} = render(
+    const { getAllByText } = render(
       <BrowserRouter>
-      <MUITable
-        columns={RenderPokemonsColumns}
+        <MUITable
+          columns={RenderPokemonsColumns}
           showPagination={false}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
           isFetching={false}
-      />
+        />
       </BrowserRouter>
     );
 
@@ -309,14 +313,14 @@ describe('MUITable', () => {
           columns={RenderPokemonsColumns}
           showPagination={false}
           data={data}
-          renderCheckbox={true} 
+          renderCheckbox={true}
           renderDeleteButton={false}
           handleChangeSelectedItems={handleChangeSelectedItems}
           isFetching={false}
         />
       </BrowserRouter>
     );
-    
+
     const checkboxes = container.querySelectorAll('input[type="checkbox"]');
 
     checkboxes.forEach((checkbox) => {
@@ -328,15 +332,15 @@ describe('MUITable', () => {
   });
 
   test('should call deleteSelectedPokemon with correct arguments when delete button is clicked', async () => {
-const deleteSelectedPokemon = jest.fn();
+    const deleteSelectedPokemon = jest.fn();
 
-    const {queryAllByTestId} = render(
+    const { queryAllByTestId } = render(
       <BrowserRouter>
         <MUITable
           columns={RenderSelectedPokemonsColumns}
           showPagination={false}
           data={data}
-          renderCheckbox={false} 
+          renderCheckbox={false}
           renderDeleteButton={true}
           handleChangeSelectedItems={handleChangeSelectedItems}
           isFetching={false}
@@ -344,11 +348,13 @@ const deleteSelectedPokemon = jest.fn();
         />
       </BrowserRouter>
     );
-    
+
     const deleteButtonsList = queryAllByTestId('delete-button');
 
-    deleteButtonsList.forEach((btn) => {
+    deleteButtonsList.forEach((btn, index) => {
       fireEvent.click(btn);
+      const pokemonId = data.pokemon_v2_pokemon[index].id;
+      expect(deleteSelectedPokemon).toHaveBeenLastCalledWith(pokemonId);
     });
 
     expect(deleteSelectedPokemon).toHaveBeenCalledTimes(deleteButtonsList.length);
@@ -356,61 +362,167 @@ const deleteSelectedPokemon = jest.fn();
   });
 
   test('navigates to Pokemon details on name click', async () => {
-    const navigate = jest.fn(); 
+    const navigate = jest.fn();
     (useNavigate as jest.Mock).mockImplementation(() => navigate);
 
 
-    const {queryAllByTestId} = render(
-          <BrowserRouter>
-            <MUITable
-              columns={RenderPokemonsColumns}
-              showPagination={false}
-              data={data}
-              renderCheckbox={false} 
-              renderDeleteButton={false}
-              handleChangeSelectedItems={handleChangeSelectedItems}
-              isFetching={false}
-            />
-          </BrowserRouter>
-        );
-        
-        const pokemonNameList = queryAllByTestId("pokemon-name");
-        pokemonNameList.forEach((pokemonName) => {
-          fireEvent.click(pokemonName);
-        })
+    const { queryAllByTestId } = render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={false}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          handleChangeSelectedItems={handleChangeSelectedItems}
+          isFetching={false}
+        />
+      </BrowserRouter>
+    );
 
-        data.pokemon_v2_pokemon.forEach((pokemon) => {
-          expect(navigate).toHaveBeenCalledWith(`/pokemon/${pokemon.id}`);
-        });
-      });
+    const pokemonNameList = queryAllByTestId("pokemon-name");
+    pokemonNameList.forEach((pokemonName) => {
+      fireEvent.click(pokemonName);
+    })
 
-      test('calls handleChangePage with correct arguments on page change', () => {
-        const handleChangePage = jest.fn();
-        const page = 1; 
-        const rowsPerPage = 10; 
-      
-        render(
-          <BrowserRouter>
-            <MUITable
-              columns={RenderPokemonsColumns}
-              showPagination={true}
-              data={data}
-              renderCheckbox={false}
-              renderDeleteButton={false}
-              handleChangeSelectedItems={handleChangeSelectedItems}
-              isFetching={false}
-              page={page}
-              rowsPerPage={rowsPerPage}
-              handleChangePage={handleChangePage}
-            />
-          </BrowserRouter>
-        );
-          
-        const nextButton = screen.getByTitle('Go to next page');
-        fireEvent.click(nextButton);
-      
-        expect(handleChangePage).toHaveBeenCalledTimes(1);
-        expect(handleChangePage).toHaveBeenCalledWith(expect.anything(), 2);
-      });
-      
+    data.pokemon_v2_pokemon.forEach((pokemon) => {
+      expect(navigate).toHaveBeenCalledWith(`/pokemon/${pokemon.id}`);
+    });
+  });
+  test('calls handleChangePage with correct arguments on page change', () => {
+    const handleChangePage = jest.fn();
+    const page = 1;
+    const rowsPerPage = 10;
+
+    render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          handleChangeSelectedItems={handleChangeSelectedItems}
+          isFetching={false}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          handleChangePage={handleChangePage}
+        />
+      </BrowserRouter>
+    );
+
+    const nextButton = screen.getByTitle('Go to next page');
+    fireEvent.click(nextButton);
+
+    expect(handleChangePage).toHaveBeenCalledTimes(1);
+    expect(handleChangePage).toHaveBeenCalledWith(expect.anything(), 2);
+  });
+
+  test('calls handleChangeRowsPerPage with correct arguments on page change', async () => {
+    const handleChangeRowsPerPage = jest.fn();
+
+    const page = 1;
+    let rowsPerPage = 10;
+
+    const { container } = render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          handleChangeSelectedItems={handleChangeSelectedItems}
+          isFetching={false}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          handleChangeRowsPerPage={handleChangeRowsPerPage}
+
+        />
+      </BrowserRouter>
+    );
+
+    const select = container.querySelector(".MuiSelect-select") as HTMLHtmlElement;
+    fireEvent.mouseDown(select);
+
+    const popup = screen.getByRole('presentation');
+    expect(popup).toBeInTheDocument();
+
+    const optionElement = screen.getByRole('option', { name: '25' });
+    userEvent.click(optionElement);
+
+    expect(handleChangeRowsPerPage).toHaveBeenCalled();
+  });
+
+  test('check numbers of pokemons', async () => {
+
+    const rowsPerPage = 10;
+
+    const { container } = render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          isFetching={false}
+          page={0}
+          rowsPerPage={rowsPerPage}
+        />
+      </BrowserRouter>
+    );
+      const listOfPokemons = container.querySelectorAll('.MuiTableBody-root tr');
+    expect(listOfPokemons.length).toBe(rowsPerPage);
+
+  });
+
+  test('render first pokemon', async () => {
+
+    const rowsPerPage = 10;
+
+    const { container } = render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          isFetching={false}
+          page={0}
+          rowsPerPage={rowsPerPage}
+        />
+      </BrowserRouter>
+    );
+    const row = container.querySelector('.MuiTableBody-root tr:first-child') as HTMLTableRowElement;
+    const firstPokemon = data.pokemon_v2_pokemon[0];
+
+    expect(row.textContent).toContain(firstPokemon.id.toString());
+    expect(row.textContent).toContain(firstPokemon.name.toString());
+    expect(row.textContent).toContain(firstPokemon.height.toString());
+    expect(row.textContent).toContain(firstPokemon.base_experience.toString());
+    expect(row.textContent).toContain(firstPokemon.is_default ? 'Yes' : 'No');
+  });
+
+  test('renders component without errors or warnings', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+    
+    render(
+      <BrowserRouter>
+        <MUITable
+          columns={RenderPokemonsColumns}
+          showPagination={true}
+          data={data}
+          renderCheckbox={false}
+          renderDeleteButton={false}
+          isFetching={false}
+          page={0}
+          rowsPerPage={10}
+        />
+      </BrowserRouter>
+    );
+    
+    expect(consoleErrorSpy).not.toHaveBeenCalled();
+  });
 });
